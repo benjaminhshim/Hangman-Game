c0b030f630e89a0fa377ec21f42c955661f15c86
Benjamin Shim <benjaminshim@Benjamins-MacBook-Pro.local>
2018-03-11 22:25:19 -0700

    Create default parameters for the start of the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,31 @@
 // CREATE AN ARRAY OF ANSWERS
+var choices = ['SUPREME', 'BAPE', 'ADIDAS', 'YEEZY', 'OFFWHITE', 'FEAROFGOD'];
 
 // CREATE GLOBAL VARIABLES
+var wins = 0;
+var guessesLeft = 10;
+var alreadyGuessed = [];
+var userGuess;
+
+var answerArray = [];
+var letters;
 
 // COMPUTER CHOOSES A RANDOM WORD
+var computerAnswer = choices[Math.floor(Math.random() * choices.length)];
+
+console.log(computerAnswer);
 
 // CREATE _ FOR EACH LETTER IN THE ANSWER
+for (var i = 0; i < computerAnswer.length; i++) {
+    answerArray[i] = '_';
+    letters = answerArray.join(' ');
+}
+
+// PRINT UNDERSCORES REPRESENTING ANSWER TO THE DOM
+document.getElementById('current-word').innerHTML = letters;
 
 // CREATE FUNCTION FOR USER'S KEY PRESS
+document.onkeyup = function (event) {
 
     // STORE USER'S KEY PRESS INTO userGuess
 
@@ -28,3 +47,6 @@
     // IF USER GUESSES THE RIGHT WORD
         // wins +1
         // COMPUTER CHOOSES A NEW RANDOM WORD
+
+
+} // end onkeyup
